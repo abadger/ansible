a0099795e7cb32845722e81c88397dc0aa54111c
Brian Coca <brian.coca+git@gmail.com>
2018-01-05 14:21:02 -0500

    correct tmpdir

--- lib/ansible/plugins/action/__init__.py
+++ lib/ansible/plugins/action/__init__.py
@@ -290,7 +290,7 @@ class ActionBase(with_metaclass(ABCMeta, object)):
         self._connection._shell.tempdir = rc
 
         if not use_system_tmp:
-            self._connection._shell.env.update({'ANSIBLE_REMOTE_TEMP': self.tempdir})
+            self._connection._shell.env.update({'ANSIBLE_REMOTE_TEMP': self._connection._shell.tempdir})
         return rc
 
     def _should_remove_tmp_path(self, tmp_path):
