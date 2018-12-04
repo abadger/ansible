5b2ad8104c9a2d5a728f54ce9cfda93b99f76ade
Toshio Kuratomi <a.badger@gmail.com>
2018-12-04 14:42:24 -0800

    [stable-2.7] Fix for callback plugins on Python3 when a module returns dictionary keys that aren't strings
    This fixes one of the problems reported in https://github.com/ansible/ansible/issues/49343
    
    Upstream Python3 bug for the json traceback:
    https://bugs.python.org/issue25457
    
    and PR that may fix it:
    https://github.com/python/cpython/pull/8011.
    (cherry picked from commit c817bef3ae39e8c2f43481c69e387776bc4e2b8c)
    
    Co-authored-by: Toshio Kuratomi <a.badger@gmail.com>

--- lib/ansible/plugins/callback/__init__.py
+++ lib/ansible/plugins/callback/__init__.py
@@ -35,11 +35,14 @@ from ansible.module_utils._text import to_text
 from ansible.utils.color import stringc
 from ansible.vars.clean import strip_internal_keys
 
-try:
-    from __main__ import display as global_display
-except ImportError:
-    from ansible.utils.display import Display
-    global_display = Display()
+if PY3:
+    # OrderedDict is needed for a backwards compat shim on Python3.x only
+    # https://github.com/ansible/ansible/pull/49512
+    from collections import OrderedDict
+else:
+    OrderedDict = None
+
+global_display = Display()
 
 try:
     from __main__ import cli
@@ -120,7 +123,18 @@ class CallbackBase(AnsiblePlugin):
         if 'exception' in abridged_result:
             del abridged_result['exception']
 
-        return json.dumps(abridged_result, cls=AnsibleJSONEncoder, indent=indent, ensure_ascii=False, sort_keys=sort_keys)
+        try:
+            jsonified_results = json.dumps(abridged_result, cls=AnsibleJSONEncoder, indent=indent, ensure_ascii=False, sort_keys=sort_keys)
+        except TypeError:
+            # Python3 bug: throws an exception when keys are non-homogenous types:
+            # https://bugs.python.org/issue25457
+            # sort into an OrderedDict and then json.dumps() that instead
+            if not OrderedDict:
+                raise
+            jsonified_results = json.dumps(OrderedDict(sorted(abridged_result.items(), key=to_text)),
+                                           cls=AnsibleJSONEncoder, indent=indent,
+                                           ensure_ascii=False, sort_keys=False)
+        return jsonified_results
 
     def _handle_warnings(self, res):
         ''' display warnings, if enabled and any exist in the result '''
